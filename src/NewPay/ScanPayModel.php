e327b8cecdee3bc6a679d5310af36865cb0e92b6
yun <1427596852@qq.com>
2023-10-09 11:52:46 +0800

    feat:扫码支付C扫B、扫码支付查询

---
+++ src/NewPay/ScanPayModel.php
@@ -0,0 +1,191 @@
+<?php
+
+namespace Sxqibo\FastPayment\NewPay;
+
+use Exception;
+use ReflectionClass;
+
+final class ScanPayModel
+{
+    const REQUEST_URL = 'https://gateway.hnapay.com/website/scanPay.do';
+
+    /** @var string 交易代码 */
+    const TRANCODE = 'WS01';
+
+    /** @var string 版本号 */
+    const VERSION = '2.1';
+
+    /** @var string 支付方式 */
+    const PAY_TYPE = 'QRCODE_B2C';
+
+    /** @var string 目标资金机构代码 */
+    /** @var string 微信支付 */
+    const ORGCODE_WECHATPAY = 'WECHATPAY';
+    /** @var string 阿里支付 */
+    const ORGCODE_ALIPAY = 'ALIPAY';
+    /** @var string 腾讯支付 */
+    const ORGCODE_TENPAY = 'TENPAY';
+    /** @var string 银联二维码 */
+    const ORGCODE_UNIONPAY = 'UNIONPAY';
+
+    /** @var int 编码方式 */
+    const CHARSET = 1;
+
+    /** @var int 签名类型 */
+    /** @var int RSA */
+    const SIGNTYPE_RSA = 1;
+
+    /** @var string[] 签名字段 */
+    const SIGN_FIELD =  ['tranCode', 'version',
+        'merId', 'submitTime', 'merOrderNum', 'tranAmt', 'payType',
+        'orgCode', 'notifyUrl', 'charset', 'signType'];
+
+    /** @var string[] 验签字段 */
+    const VERIFY_FIELD =  ['tranCode', 'version',
+        'merId', 'merOrderNum', 'tranAmt', 'submitTime', 'qrCodeUrl',
+        'hnapayOrderId', 'resultCode', 'charset', 'signType'];
+
+    private $version;
+    private $tranCode;
+    private $merId;
+    private $merOrderNum;
+    private $tranAmt;
+    private $submitTime;
+    private $payType;
+    private $orgCode;
+    private $goodsName;
+    private $goodsDetail;
+    private $tranIP;
+    private $notifyUrl;
+    private $remark;
+    private $weChatMchId;
+    private $payLimit;
+    private $identityType;
+    private $minAge;
+    private $vasType;
+    private $holderName;
+    private $identityCode;
+    private $mobileNo;
+    private $charset;
+    private $signType;
+    private $signMsg;
+
+    private $publicKey;
+    private $privateKey;
+
+    public function __construct()
+    {
+        $this->version = self::VERSION;
+        $this->tranCode = self::TRANCODE;
+        $this->submitTime = date('YmdHis', time());
+        $this->payType = self::PAY_TYPE;
+        // 此处从配置读
+        $this->tranIP = '';
+        // 此处从配置读
+        $this->notifyUrl = '';
+        $this->charset = self::CHARSET;
+        $this->signType = self::SIGNTYPE_RSA;
+    }
+
+    public function setPublicKey(string $publicKey)
+    {
+        // 收款公私钥/网关公钥（收款）.pem
+        $this->publicKey = KeyUtils::makePublicKey($publicKey);
+    }
+
+    public function setPrivateKey(string $privateKey)
+    {
+        // 收款公私钥/商户私钥（收款）.pem
+        $this->privateKey = KeyUtils::makePrivateKey($privateKey);
+    }
+
+    public function __get($name)
+    {
+        return $this->$name;
+    }
+
+    public function __set($name, $value)
+    {
+        $this->$name = $value;
+    }
+
+    /**
+     * 数据赋值
+     *
+     * @param $data
+     * @return void
+     */
+    public function copy($data)
+    {
+        foreach ($data as $key => $value) {
+            $this->$key = $value;
+        }
+    }
+
+    /**
+     * 属性转数组
+     *
+     * @return array
+     */
+    public function getDate()
+    {
+        $data = [];
+
+        $reflectionClass = new ReflectionClass(__CLASS__);
+        $reflectionProperties = $reflectionClass->getProperties();
+
+        foreach ($reflectionProperties as $property) {
+            $propertyName = $property->getName();
+            $data[$propertyName] = $this->$propertyName;
+        }
+
+        return $data;
+    }
+
+    /**
+     * 关键数据验证
+     *
+     * @return string
+     */
+    public function verify()
+    {
+        if (empty($this->merId)) {
+            return 'merId 不能为空';
+        }
+
+        if (empty($this->merOrderNum)) {
+            return 'merOrderNum 不能为空';
+        }
+
+        if (empty($this->tranAmt)) {
+            return 'tranAmt 不能为空';
+        }
+
+        if (empty($this->orgCode)) {
+            return 'orgCode 不能为空';
+        }
+
+        if (empty($this->weChatMchId)) {
+            return 'weChatMchId 不能为空';
+        }
+
+        if (!empty($this->vasType)) {
+            if (empty($this->holderName)) {
+                return 'vasType 不为空时，holderName 必填';
+            }
+            if (empty($this->identityCode)) {
+                return 'vasType 不为空时，identityCode 必填';
+            }
+            if (empty($this->mobileNo)) {
+                return 'vasType 不为空时，mobileNo 必填';
+            }
+        }
+
+        return '';
+    }
+
+    public function getSignData(): string
+    {
+        return Util::getStringData(self::SIGN_FIELD, $this->getDate());
+    }
+}
---
+++ src/NewPay/ScanPayService.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Sxqibo\FastPayment\NewPay;
+
+/**
+ * 微信&支付宝扫码（C扫B）
+ * 文档地址：https://www.yuque.com/chenyanfei-sjuaz/uhng8q/uoce7b#wZTrE
+ */
+final class ScanPayService
+{
+    /**
+     * 扫码支付
+     *
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function scanPay(ScanPayModel $scanPayModel)
+    {
+        $verify = $scanPayModel->verify();
+
+        if (!empty($verify)) {
+            return $verify;
+        }
+
+        // 对指定字段进行生成签名
+        $request = $scanPayModel->getDate();
+        $signData = $scanPayModel->getSignData();
+        $request['signMsg'] = $this->buildSign($signData, $scanPayModel->privateKey);
+
+        // post请求接口
+        $content = HttpUtil::post($request, ScanPayModel::REQUEST_URL);
+
+        // 对返回值的验签
+        $bool = $this->verifySign(json_decode($content, true), $scanPayModel->publicKey);
+        if ($bool) {
+            return $content;
+        }
+
+        return '返回值验签失败';
+    }
+
+    /**
+     * 生成签名
+     *  发送数据时的签名
+     *
+     * @param $request
+     * @param $privateKey
+     * @return string
+     * @throws \Exception
+     */
+    public function buildSign($request, $privateKey): string
+    {
+        $res = openssl_get_privatekey($privateKey);
+        openssl_sign($request, $signature, $res, OPENSSL_ALGO_SHA1);
+
+        return bin2hex($signature);
+    }
+
+    /**
+     * 接受返回数据的验签
+     *
+     * @param $data
+     * @param $publicKey
+     * @return bool
+     * @throws \Exception
+     */
+    public function verifySign($data, $publicKey): bool
+    {
+        // 生成验签字符串
+        $signParam = Util::getStringData(ScanPayModel::VERIFY_FIELD, $data);
+
+        // 验签
+        $res = openssl_get_publickey($publicKey);
+        return (bool)openssl_verify($signParam, hex2bin($data['signMsg']), $res);
+    }
+}
