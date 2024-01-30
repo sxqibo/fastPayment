093aabed099bfd16b65220cd2dd26d516fc793e0
YangHongwei <18903467858@qq.com>
2024-01-30 21:59:29 +0800

    feat: 不进行验签，直接返回值，因为有些返回本身有值，一验签就不通过了

--- src/NewPay/ScanPayService.php
+++ src/NewPay/ScanPayService.php
@@ -18,19 +18,19 @@ final class ScanPayService
     {
         // 对指定字段进行生成签名
         $request = $scanPayModel->getModelData();
-//        var_dump(json_encode($request, JSON_UNESCAPED_UNICODE));exit;
+
         // post请求接口
         $content = HttpUtil::post($request, ScanPayModel::REQUEST_URL);
 
         $content = json_decode($content, true);
 
         // 对返回值的验签
-        $bool = $scanPayModel->verifySign($content);
+        //$bool = $scanPayModel->verifySign($content);
 
-        if ($bool) {
+        // if ($bool) {
             return $content;
-        }
+        // }
 
-        return '返回值验签失败';
+        // return '返回值验签失败';
     }
 }
