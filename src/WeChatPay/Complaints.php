ad4285d81e2d991e95635c53d71a36fa28f96800
yun <1427596852@qq.com>
2024-03-08 11:42:23 +0800

    feat:修改了回复和闭关投诉的接口

--- src/WeChatPay/Complaints.php
+++ src/WeChatPay/Complaints.php
@@ -40,7 +40,7 @@ class Complaints extends BaseService
 
         // 默认查询参数
         $defaultQuery = [
-            'limit'             => 10, //【选填 integer】分页大小：设置该次请求返回的最大投诉条数，范围[1,50]
+            'limit'             => 1, //【选填 integer】分页大小：设置该次请求返回的最大投诉条数，范围[1,50]
             'offset'            => 0, //【选填 integer】分页开始位置：该次请求的分页开始位置，从0开始计数，例如offset=10，表示从第11条记录开始返回。
             'begin_date'        => date('Y-m-d'), //【必填 string(10)】开始日期：投诉发生的开始日期。注意，查询日期跨度不超过30天
             'end_date'          => date('Y-m-d'), //【必填 string(10)】结束日期：投诉发生的结束日期。注意，查询日期跨度不超过30天
@@ -121,14 +121,26 @@ class Complaints extends BaseService
         $defaultBody = [
             'complainted_mchid' => $body['complainted_mchid'], // 【被诉商户号】 投诉单对应的被诉商户号
             'response_content'  => $body['response_content'] ?? '', //【回复内容】 具体的投诉处理方案，限制200个字符以内。
-            'response_images'   => $body['response_images'] ?? [], //【回复图片】 传入调用“商户上传反馈图片”接口返回的media_id，最多上传4张图片凭证
-            'jump_url'          => $body['jump_url'] ?? '', // 【跳转链接】 商户可在回复中附加跳转链接，引导用户跳转至商户客诉处理页面，链接需满足HTTPS格式。注：配置文字链属于灰度功能, 若有需要请使用超管邮箱，按照要求发送邮件申请。邮件要求详情见：https://kf.qq.com/faq/211207a6zMBj211207ZnIr2A.html。
-            'jump_url_text'     => $body['jump_url_text'] ?? '', // 【跳转链接文案】 实际展示给用户的文案，附在回复内容之后。用户点击文案，即可进行跳转。注：若传入跳转链接，则跳转链接文案为必传项，二者缺一不可。
         ];
 
-        $body = array_merge($defaultBody, $body);
+        //【回复图片】 传入调用“商户上传反馈图片”接口返回的media_id，最多上传4张图片凭证
+        if (isset($body['response_images']) && !empty($body['response_images'])) {
+            $defaultBody['response_images'] = $body['response_images'];
+        }
 
-        $result = $this->client->requestApi($endPoint, [], $body, $this->headers, true);
+        // 【跳转链接】 商户可在回复中附加跳转链接，引导用户跳转至商户客诉处理页面，链接需满足HTTPS格式
+        // 注：配置文字链属于灰度功能, 若有需要请使用超管邮箱，按照要求发送邮件申请。
+        // 邮件要求详情见：https://kf.qq.com/faq/211207a6zMBj211207ZnIr2A.html。
+        if (isset($body['jump_url']) && !empty($body['jump_url'])) {
+            $defaultBody['jump_url'] = $body['jump_url'];
+        }
+
+        // 【跳转链接文案】 实际展示给用户的文案，附在回复内容之后。用户点击文案，即可进行跳转。注：若传入跳转链接，则跳转链接文案为必传项，二者缺一不可。
+        if (isset($body['jump_url_text']) && !empty($body['jump_url_text'])) {
+            $defaultBody['jump_url_text'] = $body['jump_url_text'];
+        }
+
+        $result = $this->client->requestApi($endPoint, [], json_encode($defaultBody, JSON_UNESCAPED_UNICODE), $this->headers, true);
 
         return $this->handleResult($result);
     }
@@ -152,9 +164,7 @@ class Complaints extends BaseService
             'complainted_mchid' => $body['complainted_mchid'], // 【被诉商户号】 投诉单对应的被诉商户号
         ];
 
-        $body = array_merge($defaultBody, $body);
-
-        $result = $this->client->requestApi($endPoint, [], $body, $this->headers, true);
+        $result = $this->client->requestApi($endPoint, [], json_encode($defaultBody, JSON_UNESCAPED_UNICODE), $this->headers, true);
 
         return $this->handleResult($result);
     }
@@ -175,16 +185,30 @@ class Complaints extends BaseService
 
         // 默认查询参数
         $defaultBody = [
-            'action'                => $body['action'] ?? 'APPROVE', // 【审批动作】 同意 或 拒绝,如果没传“同意”
-            'launch_refund_day'     => $body['launch_refund_day'] ?? 0, // 【预计发起退款时间】 在同意退款时返回，预计将在多少个工作日内能发起退款, 0代表当天
-            'reject_reason'         => $body['reject_reason'] ?? '', // 【拒绝退款原因】 在拒绝退款时返回拒绝退款的原因
-            'reject_media_list'     => $body['reject_media_list'] ?? [], // 【拒绝退款的举证图片列表】 在拒绝退款时，如果有拒绝的图片举证，可以提供 最多上传4张图片, 传入调用“商户上传反馈图片”接口返回的media_id，最多上传4张图片凭证
-            'remark'                => $body['remark'] ?? '',  // 【备注】 任何需要向微信支付客服反馈的信息
+            'action' => $body['action'] ?? 'APPROVE', // 【审批动作】 同意 或 拒绝,如果没传“同意”
         ];
 
-        $body = array_merge($defaultBody, $body);
+        // 【预计发起退款时间】 在同意退款时返回，预计将在多少个工作日内能发起退款, 0代表当天
+        if (isset($body['launch_refund_day'])
+            && (!empty($body['launch_refund_day']) || $body['launch_refund_day'] == 0)) {
+            $defaultBody['launch_refund_day'] = $body['launch_refund_day'];
+        }
 
-        $result = $this->client->requestApi($endPoint, [], $body, $this->headers, true);
+        // 【拒绝退款原因】 在拒绝退款时返回拒绝退款的原因
+        if (isset($body['reject_reason']) && !empty($body['reject_reason'])) {
+            $defaultBody['reject_reason'] = $body['reject_reason'];
+        }
+        // 【拒绝退款的举证图片列表】 在拒绝退款时，如果有拒绝的图片举证，可以提供 最多上传4张图片,
+        //  传入调用“商户上传反馈图片”接口返回的media_id，最多上传4张图片凭证
+        if (isset($body['reject_media_list']) && !empty($body['reject_media_list'])) {
+            $defaultBody['reject_media_list'] = $body['reject_media_list'];
+        }
+        // 【备注】 任何需要向微信支付客服反馈的信息
+        if (isset($body['remark']) && !empty($body['remark'])) {
+            $defaultBody['remark'] = $body['remark'];
+        }
+
+        $result = $this->client->requestApi($endPoint, [], json_encode($defaultBody, JSON_UNESCAPED_UNICODE), $this->headers, true);
 
         return $this->handleResult($result);
     }
