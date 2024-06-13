452cf2d2d5f93609c58ae2df6054d14a8e3af3f3
YangHongwei <403236160@qq.com>
2024-06-13 17:57:50 +0800

    bug: 更改统一submit

--- samples/EPay/03-mapi.php
+++ samples/EPay/03-mapi.php
@@ -10,12 +10,12 @@ $param = [
     'pid'          => $config['basic']['pid'],   // 商户ID
 
     // 2. 发起支付（获取链接）- 信息
-    'type'         => $config['order_info']['type'],            // 支付方式
-    'out_trade_no' => $config['order_info']['out_trade_no'],    // 订单号
-    'return_url'   => $config['order_info']['notify_url'],      // 异步回调地址
-    'notify_url'   => $config['order_info']['notify_url'],      // 跳转通知地址
-    'name'         => $config['order_info']['name'],            // 商品名称
-    'money'        => $config['order_info']['money'],           // 金额
+    'type'         => $config['submit_info']['type'],            // 支付方式
+    'out_trade_no' => $config['submit_info']['out_trade_no'],    // 订单号
+    'return_url'   => $config['submit_info']['notify_url'],      // 异步回调地址
+    'notify_url'   => $config['submit_info']['notify_url'],      // 跳转通知地址
+    'name'         => $config['submit_info']['name'],            // 商品名称
+    'money'        => $config['submit_info']['money'],           // 金额
 
     // 3. mapi额外的参数
     'clientip'     => $config['mapi_info']['clientip'],         // 用户IP地址
